87c7d39f045a6b160b40360e391f20c849bda466
adelmileniasucilestari <90586799+adelmileniasucilestari@users.noreply.github.com>
2021-11-15 20:56:46 +0700

    Add files via upload

--- app/src/main/java/com/example/molyst/DetailActivity.java
+++ app/src/main/java/com/example/molyst/DetailActivity.java
@@ -1,20 +1,37 @@
 package com.example.molyst;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.molyst.api.MovieModel;
+import com.example.molyst.api.ResultsItem;
+import com.example.molyst.source.ApiConfig;
+import com.example.molyst.source.ApiService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private RecyclerView scroll2;
+    private ArrayList<ResultsItem> resultsItems;
+    private Molyst molyst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        scroll2 = findViewById(R.id.scroll2);
         ImageView backdrop = findViewById(R.id.poster);
         TextView judul = findViewById(R.id.title2);
         TextView genre = findViewById(R.id.genre);
@@ -71,5 +88,32 @@ public class DetailActivity extends AppCompatActivity {
         date.setText(getIntent().getStringExtra("date"));
         lang.setText(getIntent().getStringExtra("lang"));
         overview.setText(getIntent().getStringExtra("overview"));
+
+        resultsItems = new ArrayList<>();
+        getData();
+    }
+
+    private void getData() {
+        ApiService apiService = ApiConfig.getApiService();
+        apiService.getData()
+                .enqueue(new Callback<MovieModel>() {
+                    @Override
+                    public void onResponse(Call<MovieModel> call, Response<MovieModel> response) {
+                        if (response.isSuccessful()){
+                            resultsItems = response.body().getResults();
+                            molyst = new Molyst(resultsItems, getApplicationContext());
+                            molyst.notifyDataSetChanged();
+                            scroll2.setAdapter(molyst);
+                            LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+                            llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+                            scroll2.setLayoutManager(llm);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<MovieModel> call, Throwable t) {
+
+                    }
+                });
     }
 }
